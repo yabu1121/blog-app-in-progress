109283f8365414bcfaa11f138b839bb4132a4206
yabu1121 <n.hayato0122@outlook.jp>
2026-03-01 18:40:39 +0900

    d com

--- backend/scraper/main.go
+++ backend/scraper/main.go
@@ -26,7 +26,6 @@ func main() {
 		writer.Flush()
 	}
 
-	// 2. コレクターの設定
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.navitime.co.jp"),
 		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"),
