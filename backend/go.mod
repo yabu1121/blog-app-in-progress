6756abee6520207c4eced7cef7a852195ae22ebb
yabu1121 <n.hayato0122@outlook.jp>
2026-03-01 18:40:11 +0900

    sc

--- backend/go.mod
+++ backend/go.mod
@@ -12,20 +12,36 @@ require (
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.11.0 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/antchfx/htmlquery v1.3.5 // indirect
+	github.com/antchfx/xmlquery v1.5.0 // indirect
+	github.com/antchfx/xpath v1.3.6 // indirect
+	github.com/bits-and-blooms/bitset v1.24.4 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly/v2 v2.3.0 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/nlnwa/whatwg-url v0.6.2 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
 	golang.org/x/time v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
--- backend/scraper/main.go
+++ backend/scraper/main.go
@@ -1,36 +1,85 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/gocolly/colly/v2"
 )
 
-func main () {
+func main() {
+	file, err := os.OpenFile("navitime_spots5.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("ファイルエラー: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	fInfo, _ := file.Stat()
+	if fInfo.Size() == 0 {
+		writer.Write([]string{"店名", "郵便番号", "住所", "電話番号", "備考"})
+		writer.Flush()
+	}
+
+	// 2. コレクターの設定
 	c := colly.NewCollector(
-		//スクレイピング対象のサイト
 		colly.AllowedDomains("www.navitime.co.jp"),
-		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"),
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"),
 	)
 
 	c.Limit(&colly.LimitRule{
-		// domain global
-		DomainGlob: "*navitime.co.jp*",
-		// 同時接続
+		DomainGlob:  "*navitime.co.jp*",
 		Parallelism: 1,
-		// 1 ~ 2 秒のランダム遅延
 		RandomDelay: 2 * time.Second,
 	})
 
-	c.OnHTML("li.spot-section", func(e *colly.HTMLElement){
+	detailCollector := c.Clone()
+
+	c.OnHTML("li.spot-section", func(e *colly.HTMLElement) {
 		name := e.ChildText("span.spot-name-text")
-		
-		fmt.Printf("店名: %s\n", name)
+		link := e.ChildAttr("a", "href")
+
+		if link != "" {
+			fullURL := e.Request.AbsoluteURL(link)
+			ctx := colly.NewContext()
+			ctx.Put("name", name)
+			detailCollector.Request("GET", fullURL, nil, ctx, nil)
+		}
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("訪問中:", r.URL.String())
+	detailCollector.OnHTML("body", func(e *colly.HTMLElement) {
+		name := e.Response.Ctx.Get("name")
+		var postal, address, tel, note string
+
+		e.ForEach("dl", func(_ int, el *colly.HTMLElement) {
+			dt := el.ChildText("dt")
+			dd := el.ChildText("dd")
+			switch dt {
+			case "郵便番号": postal = dd
+			case "住所": address = dd
+			case "電話番号": tel = dd
+			case "備考": note = dd
+			}
+		})
+
+		writer.Write([]string{name, postal, address, tel, note})
+		writer.Flush()
+		fmt.Printf("[保存完了] %s\n", name)
 	})
-	c.Visit("https://www.navitime.co.jp/category/0301/")
+
+	for i := 41; i <= 50; i++ {
+		targetURL := fmt.Sprintf("https://www.navitime.co.jp/category/0301/?page=%d", i)
+		fmt.Printf("\n>>> ページ %d を処理中: %s\n", i, targetURL)
+		
+		c.Visit(targetURL)
+		
+		time.Sleep(3 * time.Second)
+	}
+
+	fmt.Println("\n全10ページの処理が完了しました。")
 }
